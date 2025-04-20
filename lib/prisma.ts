e6592e5aed82ad0822e2c80c7dbd91484c7b1019
shanmukavenkat <21bq1a4208@vvit.net>
2025-04-20 11:43:36 +0530

    snvs

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,12 @@
 import { PrismaClient } from '@prisma/client'
 import { Pool, neonConfig } from '@neondatabase/serverless'
+import { PrismaNeon } from '@prisma/adapter-neon'
 import ws from 'ws'
+
+// Ensure DATABASE_URL is set
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set in environment variables')
+}
 
 // Setup Neon WebSocket
 neonConfig.webSocketConstructor = ws
@@ -8,13 +14,12 @@ neonConfig.webSocketConstructor = ws
 // Setup Neon Pool
 const pool = new Pool({ connectionString: process.env.DATABASE_URL })
 
-// Setup Prisma Client with Neon driver
+// Setup Prisma Neon Adapter
+const adapter = new PrismaNeon(pool)
+
+// Setup Prisma Client with Neon driver adapter
 const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
+  adapter,
 })
 
 export default prisma
