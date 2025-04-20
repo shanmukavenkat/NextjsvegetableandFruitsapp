0ae27ccea38c5abe7c1dd82ac119c4c09db9fc50
shanmukavenkat <21bq1a4208@vvit.net>
2025-04-20 20:25:06 +0530

    snvs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint:{
+    ignoreDuringBuilds: true, // Ignore ESLint errors during build
+
+  },
     images: {
       remotePatterns: [
         {
